e29cef962bb6891cef5726ff1b061cd5c0dc6dd1
Bobonimo111 <carlosmaycon443@gmail.com>
2022-07-14 08:19:19 -0300

    Desafio073 iniciado

---
+++ Aula 16 tuplas/Desafio073.py
@@ -0,0 +1,7 @@
+# Em uma tupla colocar os 20 primeiros colocados do campeonato brasileiro de futebol
+# Retorna
+# A) os cinco primeiros colocados
+# B) Os 4 ultimo colocados 
+# C) Uma lista em ordem alfabetica
+# D) Em que posição está a chapecoence
+Cbraisileiro = ('Palmeiras','Corinthians','Internacional','Atlético Mineiro','Fluminense','Athletico-PR','São Paulo','Santos','Flamengo','Botafogo','Bragantino','Goiás','Cuiabá','Coritiba','América-MG','Avaí','Ceará','Atrético Goianiense','juventude','Fortaleza')
