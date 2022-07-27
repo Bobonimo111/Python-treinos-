de32aa8e447b89db0a36494e1055509a55e7ae60
Bobonimo111 <carlosmaycon443@gmail.com>
2022-07-27 13:41:41 -0300

    Desafio080 incompleto

---
+++ Aula 17 Listas/Desafio080.py
@@ -0,0 +1,9 @@
+# Entrar um valor
+# Entrar direto na ordem crescente da lista
+# retornar qual a posição que ele foi adicionado 
+
+MyList = []
+MyList.append(int(input('Digite um numero: ')))
+print('Adicionado ao final...')
+
+print(MyList)
