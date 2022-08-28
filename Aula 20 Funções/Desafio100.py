35a1cf7ab064da905571e914e6cbd2e84dacaed6
Bobonimo111 <carlosmaycon443@gmail.com>
2022-08-28 12:57:55 -0300

    Desafio100 \n -completo \n sem resolução de video

---
+++ Aula 20 Funções/Desafio100.py
@@ -0,0 +1,28 @@
+# Criar uma função que retorna uma lista
+# Criar uma função que recebe uma lista e soma os numeros pares 
+from time import sleep
+from random import randint
+
+fps = 0.5
+
+def Sorteia(quant):
+    Rand_lista = list()
+    print(f'Sorteando {quant} valores da lista: ',end='')
+    for i in range(0,quant):
+        rad = randint(0,10)
+        print(f'{rad} ',end='',flush=True)
+        sleep(fps)
+        Rand_lista.append(rad)
+    print('PRONTO!')
+    return Rand_lista
+
+
+def SomaPar(nuns):
+    soma = 0
+    for n in nuns:
+        if(n%2 == 0):
+            soma = soma + n
+    print(f'Somando o valores pares de, {nuns}, temos {soma}.')
+
+
+SomaPar(Sorteia(5))
